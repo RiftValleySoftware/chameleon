dd4c7d98fed235b752bdf365f2761a72f33a1bd2
Chris M <webmaster@magnaws.com>
2018-04-05 08:47:17 -0400

    Added support methods

--- badger_extension_classes/co_us_place_collection.class.php
+++ badger_extension_classes/co_us_place_collection.class.php
@@ -21,4 +21,74 @@ require_once(CO_Config::db_classes_extension_class_dir().'/co_us_place.class.php
 This is a specialization of the location class. It adds support for US addresses, and uses the first eight tags for this.
  */
 class CO_US_Place_Collection extends CO_US_Place implements iCO_Collection {
+    var $container;
+    var $position;
+    
+    /// CO_US_Place_Collection Methods
+    public function __construct() {
+        $this->container = Array();
+        $this->rewind() = 0;
+    }
+    
+    /// Iterator Methods
+    public mixed current() {
+        return $this->offsetExists($this->key()) ? $this->container[$this->key()] : NULL;
+    }
+    
+    public scalar key() {
+        return $this->position;
+    }
+    
+    public void next() {
+        if ($this->position < (count($this->container) + 1)) {
+            ++$this->position;
+        }
+    }
+    
+    public void rewind() {
+        $this->position = 0;
+    }
+    
+    public bool valid () {
+        return isset($this->container[$this->position]);
+    }
+    
+    
+    /// ArrayAccess Methods
+    public bool offsetExists(mixed $offset) {
+        return isset($this->container[$offset]);
+    }
+    
+    public mixed offsetGet(mixed $offset) {
+        return isset($this->container[$offset]) ? $this->container[$offset] : NULL;
+    }
+    
+    public void offsetSet(mixed $offset , mixed $value) {
+        if (!$this->isElementInHierarchy($value)) {
+            if (is_null($offset)) {
+                $this->container[] = $value;
+            } else {
+                $this->container[$offset] = $value;
+            }
+        }
+    }
+    
+    public void offsetUnset(mixed $offset) {
+        if ($this->offsetExists($offset)) {
+            unset($this->container[$offset]);
+        }
+    }
+    
+    /// iCO_Collection Methods
+    public bool appendElement(mixed $in_element) {
+    }
+    
+    public bool appendElements([mixed $in_element]) {
+    }
+    
+    public bool isElementInHierarchy(mixed $in_element) {
+    }
+    
+    public [mixed] getHierarchy(mixed $in_element) {
+    }
 };
--- badger_extension_classes/ico_collection.interface.php
+++ badger_extension_classes/ico_collection.interface.php
@@ -18,5 +18,9 @@ require_once(CO_Config::db_classes_class_dir().'/co_ll_location.class.php');
 /***************************************************************************************************************************/
 /**
  */
-interface iCO_Collection {
+interface iCO_Collection extends Iterator extends ArrayAccess {
+    abstract public bool appendElement(mixed $in_element)
+    abstract public bool appendElements([mixed $in_element])
+    abstract public bool isElementInHierarchy(mixed $in_element)
+    abstract public [mixed] getHierarchy(mixed $in_element)
 };
