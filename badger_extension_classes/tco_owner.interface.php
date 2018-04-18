fb8a4c8dd2ad1441b2e66982606392ade1948655
Chris M <webmaster@magnaws.com>
2018-04-18 13:07:08 -0400

    Adding a new owner interface class. More will be revealed...

---
+++ badger_extension_classes/tco_owner.interface.php
@@ -0,0 +1,28 @@
+<?php
+/***************************************************************************************************************************/
+/**
+    Badger Hardened Baseline Database Component
+    
+    © Copyright 2018, Little Green Viper Software Development LLC.
+    
+    This code is proprietary and confidential code, 
+    It is NOT to be reused or combined into any application,
+    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+
+    Little Green Viper Software Development: https://littlegreenviper.com
+*/
+defined( 'LGV_DBF_CATCHER' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
+
+require_once(CO_Config::db_classes_class_dir().'/co_ll_location.class.php');
+
+/***************************************************************************************************************************/
+/**
+This is a trait for the basic "owner" aggregator functionality.
+ */
+trait tCO_Owner {
+    /***********************/
+    /**
+     */
+    public function getAllOwnedObjects() {
+    }
+}
