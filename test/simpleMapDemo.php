8c828c6a7f9f469dbb709013ee1bbebd199d4469
Chris M <webmaster@magnaws.com>
2018-04-19 07:23:07 -0400

    Fixed a couple of errors, caused by my removing the import of the config file in the functions file.

--- test/simpleMapDemo.php
+++ test/simpleMapDemo.php
@@ -11,6 +11,15 @@
 
     Little Green Viper Software Development: https://littlegreenviper.com
 */
+
+$config_file_path = dirname(__FILE__).'/config/s_config.class.php';
+
+if ( !defined('LGV_CONFIG_CATCHER') ) {
+    define('LGV_CONFIG_CATCHER', 1);
+}
+
+require_once($config_file_path);
+
 require_once(dirname(__FILE__).'/functions.php');
 
 if ( !defined('LGV_ACCESS_CATCHER') ) {
