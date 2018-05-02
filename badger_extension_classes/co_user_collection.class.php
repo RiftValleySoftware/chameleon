9f3af3ef00a27e4ee9d37af6e6c10eb33871cb54
Chris M <webmaster@magnaws.com>
2018-05-02 17:20:02 -0400

    Added a user collection class. It isn't done yet.

---
+++ badger_extension_classes/co_user_collection.class.php
@@ -0,0 +1,127 @@
+<?php
+/***************************************************************************************************************************/
+/**
+    CHAMELEON Object Abstraction Layer
+    
+    © Copyright 2018, Little Green Viper Software Development LLC.
+    
+    This code is proprietary and confidential code, 
+    It is NOT to be reused or combined into any application,
+    unless done so, specifically under written license from Little Green Viper Software Development LLC.
+
+    Little Green Viper Software Development: https://littlegreenviper.com
+*/
+defined( 'LGV_DBF_CATCHER' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
+
+CO_Config::require_extension_class('tco_collection.interface.php');
+require_once(CO_Config::db_class_dir().'/co_main_db_record.class.php');
+
+$lang_file = CO_Config::badger_lang_class_dir().'/'.$lang.'.php';
+$lang_common_file = CO_Config::badger_lang_class_dir().'/common.inc.php';
+
+if ( !defined('LGV_LANG_CATCHER') ) {
+    define('LGV_LANG_CATCHER', 1);
+}
+
+require_once($lang_file);
+require_once($lang_common_file);
+
+/***************************************************************************************************************************/
+/**
+This is a container, meant to define a user.
+ */
+class CO_User_Collection extends CO_Main_DB_Record {
+    private $_login_object = NULL;  ///< The Security DB COBRA login instance associated with this user.
+
+    /***********************/
+    /**
+    \returns TRUE, if the instance was able to set itself up to the internal login ID.
+     */
+    protected function _load_login() {
+        $ret = FALSE;
+        
+        // Tag 0 contains the ID of the user login (in the security DB) for this user.
+        $login_id = intval($this-itags[0]);
+        
+        if (0 < $login_id) {
+            $my_login_object = $this->get_access_object()->get_single_security_record_by_id($login_id);
+            
+            if (isset($my_login_object) && ($my_login_object instanceof CO_Security_Login)) {
+                $this->_login_object = $my_login_object;
+                $ret = TRUE;
+            } elseif (!($my_login_object instanceof CO_Security_Login)) {
+                $this->error = new LGV_Error(   CO_Lang_Common::$user_error_code_invalid_class,
+                                                CO_Lang::$user_error_name_invalid_class,
+                                                CO_Lang::$user_error_desc_invalid_class,
+                                                __FILE__,
+                                                __LINE__,
+                                                __METHOD__
+                                            );
+            } else {
+                $this->error = new LGV_Error(   CO_Lang_Common::$user_error_code_user_not_authorized,
+                                                CO_Lang::$user_error_name_user_not_authorized,
+                                                CO_Lang::$user_error_desc_user_not_authorized,
+                                                __FILE__,
+                                                __LINE__,
+                                                __METHOD__
+                                            );
+            }
+        } else {
+            $this->error = new LGV_Error(   CO_Lang_Common::$user_error_code_invalid_id,
+                                            CO_Lang::$user_error_name_invalid_id,
+                                            CO_Lang::$user_error_desc_invalid_id,
+                                            __FILE__,
+                                            __LINE__,
+                                            __METHOD__
+                                        );
+        }
+        
+        return $ret;
+    }
+    
+    /***********************************************************************************************************************/
+    /***********************/
+    /**
+    Constructor (Initializer)
+     */
+	public function __construct(    $in_db_object = NULL,   ///< The database object for this instance.
+	                                $in_db_result = NULL,   ///< The database row for this instance (associative array, with database keys).
+	                                $in_owner_id = NULL,    ///< The ID of the object (in the database) that "owns" this instance.
+	                                $in_tags_array = NULL   ///< An array of strings, up to ten elements long, for the tags. Tag 0 MUST be a single integer (as a string), with the ID of the login object associated with this instance.
+                                ) {
+        
+        $this->_container = Array();
+        $this->_login_object = NULL;
+        
+        parent::__construct($in_db_object, $in_db_result, $in_owner_id, $in_tags_array);
+        
+        $this->class_description = "This is a 'Collection' Class for Users.";
+    }
+    
+    /***********************/
+    /**
+    This function sets up this instance, according to the DB-formatted associative array passed in.
+    
+    \returns TRUE, if the instance was able to set itself up to the provided array.
+     */
+    public function load_from_db(   $in_db_result   ///< This is an associative array, formatted as a database row response.
+                                    ) {
+        $ret = parent::load_from_db($in_db_result);
+        
+        if ($this->_load_login()) {
+            $this->_set_up_container();
+        }
+        
+        $this->class_description = "This is a 'Collection' Class for Users.";
+    }
+    
+    /***********************/
+    /**
+     Accessor for the login object.
+     */
+    public function get_login_instance() {
+        return $this->_login_object;
+    }
+    
+    use tCO_Collection; ///< These are the built-in collection methods.
+};
