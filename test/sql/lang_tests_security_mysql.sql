16076173596b3658c388fdc82efbc83f67d6babe
Chris M <webmaster@magnaws.com>
2018-05-11 17:50:23 -0400

    OK. Fixed the primer SQL to remove COBRA references.

--- test/sql/lang_tests_security_mysql.sql
+++ test/sql/lang_tests_security_mysql.sql
@@ -16,12 +16,7 @@ INSERT INTO `co_security_nodes` (`id`, `login_id`, `access_class`, `last_access`
 (2, 'admin', 'CO_Security_Login', '1970-01-01 00:00:00', 2, 2, 'Default Admin', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:4:"JUNK";}', NULL),
 (3, 'norm', 'CO_Security_Login', '1970-01-01 00:00:00', 3, 3, 'Low-Level 1', 'a:2:{s:4:"lang";s:2:"fr";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL),
 (4, 'bob', 'CO_Security_Login', '1970-01-01 00:00:00', 4, 4, 'Low-Level 2', 'a:2:{s:4:"lang";s:2:"fr";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL),
-(5, 'cobra', 'CO_Cobra_Login', '1970-01-01 00:00:00', 5, 5, 'Normal COBRA Login (cobra)', 'a:1:{s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL),
-(6, 'krait', 'CO_Cobra_Login', '1970-01-01 00:00:00', 6, 6, 'Normal COBRA Login (krait)', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '2, 10'),
-(7, 'asp', 'CO_Login_Manager', '1970-01-01 00:00:00', 7, 7, 'Boss COBRA Manager (asp)', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '3,4,5'),
-(8, 'king-cobra', 'CO_Login_Manager', '1970-01-01 00:00:00', 8, 8, 'Boss COBRA Manager (king)', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '2,6,10'),
-(9, 'duke', 'CO_Login_Manager', '1970-01-01 00:00:00', 9, 9, 'Daisy', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '3,10'),
-(10, 'emperor', 'CO_Login_Manager', '1970-01-01 00:00:00', 10, 10, 'Me Big Chief', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '2,3,4,5,6,7,8,9');
+(5, 'cobra', 'CO_Security_Login', '1970-01-01 00:00:00', 5, 5, 'Normal COBRA Login (cobra)', 'a:1:{s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL);
 
 ALTER TABLE `co_security_nodes`
   ADD PRIMARY KEY (`id`),
@@ -33,4 +28,4 @@ ALTER TABLE `co_security_nodes`
   ADD KEY `object_name` (`object_name`);
 
 ALTER TABLE `co_security_nodes`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
--- test/sql/lang_tests_security_pgsql.sql
+++ test/sql/lang_tests_security_pgsql.sql
@@ -18,9 +18,4 @@ INSERT INTO co_security_nodes (login_id, access_class, last_access, read_securit
 ('admin', 'CO_Security_Login', '1970-01-01 00:00:00', 2, 2, 'Default Admin', 'a:2:{s:4:"lang";s:2:"en";s:15:\"hashed_password\";s:4:\"JUNK\";}', NULL),
 ('norm', 'CO_Security_Login', '1970-01-01 00:00:00', 3, 3, 'Low-Level 1', 'a:2:{s:4:"lang";s:2:"fr";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL),
 ('bob', 'CO_Security_Login', '1970-01-01 00:00:00', 4, 4, 'Low-Level 2', 'a:2:{s:4:"lang";s:2:"fr";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL),
-('cobra', 'CO_Cobra_Login', '1970-01-01 00:00:00', 5, 5, 'Normal COBRA Login (cobra)', 'a:1:{s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL),
-('krait', 'CO_Cobra_Login', '1970-01-01 00:00:00', 6, 6, 'Normal COBRA Login (krait)', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '2,10'),
-('asp', 'CO_Login_Manager', '1970-01-01 00:00:00', 7, 7, 'Boss COBRA Manager (asp)', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '3,4,5'),
-('king-cobra', 'CO_Login_Manager', '1970-01-01 00:00:00', 8, 8, 'Boss COBRA Manager (king)', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '2,6,10'),
-('duke', 'CO_Login_Manager', '1970-01-01 00:00:00', 9, 9, 'Daisy', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '3,10'),
-('emperor', 'CO_Login_Manager', '1970-01-01 00:00:00', 10, 10, 'Me Big Chief', 'a:2:{s:4:"lang";s:2:"en";s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', '2,3,4,5,6,7,8,9');
+('cobra', 'CO_Security_Login', '1970-01-01 00:00:00', 5, 5, 'Normal COBRA Login (cobra)', 'a:1:{s:15:"hashed_password";s:13:"62oAxuuttfjZI";}', NULL);
