3f8ebc9819508fec7d9757129967d853f8094edd
Chris Marshall <chris@riftvalleysoftware.com>
2020-12-15 13:22:12 -0500

    Added support for a query to get login information.

--- main/co_chameleon.class.php
+++ main/co_chameleon.class.php
@@ -25,7 +25,7 @@
 */
 defined( 'LGV_ACCESS_CATCHER' ) or die ( 'Cannot Execute Directly' );	// Makes sure that this file is in the correct context.
 
-define('__CHAMELEON_VERSION__', '1.0.6.3000');
+define('__CHAMELEON_VERSION__', '1.0.7.3000');
 
 require_once(CO_Config::badger_main_class_dir().'/co_access.class.php');
 
@@ -349,6 +349,14 @@ class CO_Chameleon extends CO_Access {
      */
     public function get_all_visible_users() {
         return $this->_data_db_object->get_all_visible_users();
+    }
+
+    /***********************/
+    /**
+    \returns a simple associative array, indexed by login (not user) IDs, with each element being an array, containing the name (usually display name) of the login object in [0], and the login ID, in [1]. It reurns ALL logins visible to the current login.
+     */
+    public function get_all_visible_logins() {
+        return $this->_security_db_object->get_all_visible_users();
     }
     
     /***********************/
