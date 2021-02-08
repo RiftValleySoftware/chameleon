c04d5d7b851f28b4b5cd654e28a806c21136ef88
Chris Marshall <chris@riftvalleysoftware.com>
2021-02-08 14:47:02 -0500

    Just tweaked the config and the display of the test main page.

--- test/config/s_config.class.php
+++ test/config/s_config.class.php
@@ -39,6 +39,7 @@ class CO_Config {
     static $min_pw_len = 8;                         ///< The minimum password length.
     static $session_timeout_in_seconds = 2;         ///< Two-Second API key timeout.
     static $god_session_timeout_in_seconds  = 1;    ///< API key session timeout for the "God Mode" login, in seconds (integer value). Default is 10 minutes.
+    static $use_personal_tokens = true;             ///< If TRUE, then we can use "personal IDs."
     
     static $data_db_name = 'littlegr_badger_data';
     static $data_db_host = 'localhost';
